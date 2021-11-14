2261dd5c822c2caeb4cc2b6b609152bf13725af3
yeng4011 <89770222+yeng4011@users.noreply.github.com>
2021-11-15 04:13:42 +0800

    Create Class03.java

---
+++ Class03.java
@@ -0,0 +1,44 @@
+package Class03.java;
+
+class Caaa
+ {
+ public int num1;
+ public int num2;
+
+ public Caaa()
+ {
+ num1=1;
+ num2=1;
+ }
+ public Caaa(int a,int b)
+ {
+ num1=a;
+ num2=b;
+ }
+ }
+
+ class Cbbb extends Caaa
+ {
+ public Cbbb()
+ {}
+ public Cbbb(int a,int b)
+ {
+ super(a,b);
+ }
+ public void show()
+ {
+ System.out.println("num1="+num1+", num2="+num2);
+ }
+ } 
+
+ public class Class03
+ {
+ public static void main(String args[])
+ {
+ Cbbb b1=new Cbbb();
+ Cbbb b2=new Cbbb(4,8);
+
+ b1.show();
+ b2.show();
+ }
+ } 
